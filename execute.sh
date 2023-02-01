e88f25f99a2d935b445f0eeaf09737076e844a56
Nitsan Avni <nitsanav@gmail.com>
2023-02-01 20:58:14 +0000

    hello world

--- execute.sh
+++ execute.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-# jenny
-
-./jenny -n2 -s5 4 6 6 | grep -v Could | (echo const int jenny[][3] = {; sed -f indices-jenny.sed;echo }\;;) > jenny.h
-
-# clean coverage
-rm gilded-rose.gc*
-rm gilded-rose-test.gc*
-
 # build
 cc \
-    -ftest-coverage -fprofile-arcs \
-    gilded-rose-test.c src/gilded-rose.c -o gilded-rose-test > /dev/null
+    gilded-rose-test.c -o gilded-rose-test > /dev/null
 
-# run & test
-./gilded-rose-test | ./verify.sh -t gilded-rose
+# run
+./gilded-rose-test
 
 # clean
 rm gilded-rose-test
-
-# coverage
-gcov -b gilded-rose.c
-# gcov gilded-rose.c
-
