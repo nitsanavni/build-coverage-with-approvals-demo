150fda45218f878f33d622fee011f981ad470c11
Nitsan Avni <nitsanav@gmail.com>
2023-02-01 17:37:53 +0000

    100%, 6 mutants survive

--- execute.sh
+++ execute.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# jenny
+
+./jenny -n2 -s1 4 6 6 | grep -v Could | (echo const int jenny[][3] = {; sed -f indices-jenny.sed;echo }\;;) > jenny.h
+
 # build
 cc \
     -ftest-coverage -fprofile-arcs \
--- gilded-rose-test.c
+++ gilded-rose-test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "./jenny.h"
 #include "./src/gilded-rose.h"
 
 #define S(array) (sizeof(array) / sizeof((array)[0]))
@@ -14,17 +15,26 @@ int main() {
   int sellIns[] = {0, -1, 12, 1, 11, 6};
   int qualities[] = {0, 1, 2, 49, 50, 51};
 
-  for (const char **name = names; name < E(names); name++) {
-    for (int *sellIn = sellIns; sellIn < E(sellIns); sellIn++) {
-      for (int *quality = qualities; quality < E(qualities); quality++) {
-        init_item(&item, *name, *sellIn, *quality);
+  //   for (const char **name = names; name < E(names); name++) {
+  //     for (int *sellIn = sellIns; sellIn < E(sellIns); sellIn++) {
+  //       for (int *quality = qualities; quality < E(qualities); quality++) {
+  //         init_item(&item, *name, *sellIn, *quality);
 
-        printf("%s %d %d -> ", item.name, item.sellIn, item.quality);
+  //         printf("%s %d %d -> ", item.name, item.sellIn, item.quality);
 
-        update_quality(&item, 1);
+  //         update_quality(&item, 1);
 
-        printf("%d %d\n", item.sellIn, item.quality);
-      }
-    }
+  //         printf("%d %d\n", item.sellIn, item.quality);
+  //       }
+  //     }
+  //   }
+
+  for (const int(*j)[3] = jenny; j < E(jenny); j++) {
+    Item item;
+    init_item(&item, names[(*j)[0]], sellIns[(*j)[1]], qualities[(*j)[2]]);
+
+    printf("%s %d %d -> ", item.name, item.sellIn, item.quality);
+    update_quality(&item, 1);
+    printf("%d %d\n", item.sellIn, item.quality);
   }
 }
