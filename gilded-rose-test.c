f962b5480b2392a662e95082422e8bc912ab9b60
Nitsan Avni <nitsanav@gmail.com>
2023-02-01 21:20:37 +0000

    cover line 21 too

--- gilded-rose-test.c
+++ gilded-rose-test.c
@@ -12,4 +12,12 @@ int main() {
   update_quality(&item, 1);
 
   printf("%d %d\n", item.sellIn, item.quality);
+
+  init_item(&item, "name", 0, 1);
+
+  printf("%s %d %d -> ", item.name, item.sellIn, item.quality);
+
+  update_quality(&item, 1);
+
+  printf("%d %d\n", item.sellIn, item.quality);
 }
