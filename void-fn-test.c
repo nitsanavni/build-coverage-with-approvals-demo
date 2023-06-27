fd44795d7b38f021c19e61d89526f093cdc1e6f1
Nitsan Avni <nitsanav@gmail.com>
2023-06-27 13:47:08 +0000

    set globals

--- void-fn-test.c
+++ void-fn-test.c
@@ -6,6 +6,36 @@
 #define S(array) (sizeof(array) / sizeof((array)[0]))
 #define E(array) (array + S(array))
 
+void set_globals(int _AnsprAufO, int _AnsprAufV, int _AnsprBand, int _AnsprHyst,
+                 int _AnsprZuO, int _AnsprZuV, unsigned int _AutoIbsOk,
+                 unsigned int _BinSteuer, int _Nerker1, unsigned int _NImpuls,
+                 unsigned int _NRegFkt, int _RegDiff, int _RegDiffSch,
+                 unsigned int _RegMode, int _SollwertRev, int _StellFwd,
+                 int _StellIstRev, int _WirkFall) {
+  AnsprAufO = _AnsprAufO;
+  AnsprAufV = _AnsprAufV;
+  AnsprBand = _AnsprBand;
+  AnsprHyst = _AnsprHyst;
+  AnsprZuO = _AnsprZuO;
+  AnsprZuV = _AnsprZuV;
+  AutoIbsOk = _AutoIbsOk;
+  BinSteuer = _BinSteuer;
+  Nerker1 = _Nerker1;
+  NImpuls = _NImpuls;
+  NRegFkt = _NRegFkt;
+  RegDiff = _RegDiff;
+  RegDiffSch = _RegDiffSch;
+  RegMode = _RegMode;
+  SollwertRev = _SollwertRev;
+  StellFwd = _StellFwd;
+  StellIstRev = _StellIstRev;
+  WirkFall = _WirkFall;
+}
+
+void reset_globals() {
+  set_globals(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+}
+
 void print_em_globals() {
   printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", AnsprAufO,
          AnsprAufV, AnsprBand, AnsprHyst, AnsprZuO, AnsprZuV, AutoIbsOk,
@@ -21,5 +51,18 @@ int main() {
   theFunctionToTest();
 
   // after
+  print_em_globals();
+
+  theFunctionToTest();
+
+  print_em_globals();
+
+  printf("resetting\n");
+  reset_globals();
+
+  print_em_globals();
+
+  theFunctionToTest();
+
   print_em_globals();
 }
